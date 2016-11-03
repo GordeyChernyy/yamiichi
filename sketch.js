57276df8fa13f572c24979b34c34a2fce1ba7ca3
gordey <kewava@gmail.com>
2016-11-03 12:06:24 -0400

    add parameters to link

--- sketch.js
+++ sketch.js
@@ -1,26 +1,56 @@
 var pts = [];
-
+var data = [];
+var counter = 0;
 function preload(){
-	
+	var id = urlParam('portraitId');
+	print(id);
+	if(id == 'sun'){
+		data = loadJSON("assets/sun.json");
+	}else if(id == 'eye'){
+		data = loadJSON("assets/eye.json");
+	}
+	console.log(data.length);
 }
-
+var urlParam = function(name, w){
+    w = w || window;
+    var rx = new RegExp('[\&|\?]'+name+'=([^\&\#]+)'),
+        val = w.location.search.match(rx);
+    return !val ? '':val[1];
+}
 function setup(){
-   createCanvas( windowWidth, windowHeight );
-   clearPts();
-  background(255);
+   	createCanvas( windowWidth, windowHeight );
+	clearPts();
+	background(255);
 }
+function draw(){
+	// events
+	if(data[counter]['click'] == true){
+		console.log(data[counter]['click']);
+		clearPts();
+	}
 
+	// draw
+	drawBrush(data[counter]['x'], data[counter]['y']);
+	
+	// counter
+	counter++;
+	if(counter > Object.keys(data).length-1){
+		counter = 0;
+		clear();
+		console.log(counter);
+	}
+}
 function clearPts() {
-  pts =[];
+	pts =[];
 }
 
 function mousePressed(){
   
 }
 
-function mouseDragged(){
-  var mouseV = createVector(mouseX, mouseY);
-  pts.push(createVector(mouseX, mouseY));
+function drawBrush(x, y){
+  var mouseV = createVector(x, y);
+  pts.push(createVector(x, y));
   stroke(0, 30);
   for(var i = 0; i < pts.length; i++){
     if (mouseV.dist(pts[i])/200 < random(0.4)) {
